65511a47df140b9afc012c7f69f77ca512831354
richardjorne <154006102+richardjorne@users.noreply.github.com>
2024-05-08 15:51:57 +0800

    Update Package

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "StateViewSPM",
+    name: "StateView",
     platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "StateViewSPM",
-            targets: ["StateViewSPM"]),
+            name: "StateView",
+            targets: ["StateView"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "StateViewSPM"),
+            name: "StateView"),
         .testTarget(
-            name: "StateViewSPMTests",
-            dependencies: ["StateViewSPM"]),
+            name: "StateViewTests",
+            dependencies: ["StateView"]),
     ]
 )
